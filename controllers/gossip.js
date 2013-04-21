9441e5975fc1d2f267d77110595c218eabc910c8
巴思 <basi.dwz@taobao.com>
2013-04-22 01:19:52 +0800

    修改文案

--- controllers/gossip.js
+++ controllers/gossip.js
@@ -20,5 +20,5 @@ var list = [
 
 exports.handle = function (message, req, res) {
   var createTime = message.CreateTime || 0;
-  res.reply(list[createTime % list.length] + "\n难道我会告诉你八卦是随机出的，多次问我我会说不同的八卦吗！");
+  res.reply(list[createTime % list.length] + "\n（八卦很多，随机出的）");
 };
