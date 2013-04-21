34ccdf023c2cd3f911d1a2c0f32435eb32884712
巴思 <basi.dwz@taobao.com>
2013-04-21 17:46:44 +0800

    add google map

--- controllers/location.js
+++ controllers/location.js
@@ -15,7 +15,7 @@ exports.handle = function (req, res) {
   res.reply([{
     title: '江干区钱潮路天元大厦',
     description: '点我查看更酒店详细地址和驾车路线',
-    picurl: config.serverHost + '/assets/img/location.jpg',
+    picurl: config.serverHost + '/assets/img/location.png',
     url: config.serverHost + '/static/location'
   }]);
 };
